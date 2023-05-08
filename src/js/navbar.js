3fe442b75619520cc509e856604278991d198c4a
Chiro28 <rv.jassir@gmail.com>
2023-05-08 14:44:56 -0500

    Modificando navbar

---
+++ src/js/navbar.js
@@ -0,0 +1,10 @@
+var nav_button = document.getElementById('boton-menu');
+var nav_ul = document.getElementById('nav-flex-item nav-links');
+
+// Add click event listeners to the buttons
+nav_button.addEventListener('click', function() {
+  div1.style.display = 'flex';
+  div2.style.display = 'none';
+  div3.style.display = 'none';
+});
+
