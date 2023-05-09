069e5699b43b2f5a38b37548b69fe84a1c17d936
JairUribeAlvites <jair.urvites@gmail.com>
2023-05-09 00:02:19 -0500

    button effects

--- src/js/smallnavbar.js
+++ src/js/smallnavbar.js
@@ -23,3 +23,34 @@ button3.addEventListener('click', function() {
   div2.style.display = 'none';
   div3.style.display = 'flex';
 });
+
+const btn = document.querySelectorAll(".small-navbar button");
+W// Listen for click event
+for (let i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (e) {
+
+      // Create span element
+      let ripple = document.createElement("span");
+
+      // Add ripple class to span
+      ripple.classList.add("ripple");
+
+      // Add span to the button
+      this.appendChild(ripple);
+
+      // Get position of X
+      let x = e.clientX - e.target.offsetLeft;
+      // Get position of Y
+      let y = (e.pageY-document.querySelector('#sobremi').offsetHeight-document.querySelector('header').offsetHeight) - (e.target.offsetTop-document.querySelector('#sobremi').offsetHeight-document.querySelector('header').offsetHeight);
+
+      // Position the span element
+      ripple.style.left = `${x}px`;
+      ripple.style.top = `${y}px`;
+
+      // Remove span after 0.3s
+      setTimeout(() => {
+          ripple.remove();
+      }, 300);
+
+  };
+};
